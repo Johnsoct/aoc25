87a47c73d4a35bce5c327120bcddc9c944efe50c
Taylor <johnsoct@hey.com>
2025-12-03 04:51:09 -0500

    d3p1 complete

---
+++ 3/3a.ts
@@ -0,0 +1,29 @@
+import puzzleData from "./3.json" with { type: "json" };
+
+const batteryBanks = puzzleData.batteryBanks;
+const batteryBanksMaxOutputs: number[] = [];
+
+batteryBanks.forEach((batteryBank) => {
+    const joltages = batteryBank.split("");
+    let highCombination: number = 0;
+
+    // Two pointers - brute forcing
+    for (let left = 0; left < joltages.length; left++) {
+        for (let right = left + 1; right < joltages.length; right++) {
+            const joltage = Number(joltages[left] + joltages[right]);
+
+            highCombination = Math.max(highCombination, joltage);
+        };
+    }
+
+    batteryBanksMaxOutputs.push(highCombination);
+});
+
+const maxOutput = batteryBanksMaxOutputs.reduce((acc: number, cur: number) => {
+    // eslint-disable-next-line no-param-reassign
+    acc += cur;
+
+    return acc;
+}, 0);
+
+console.log(maxOutput);
