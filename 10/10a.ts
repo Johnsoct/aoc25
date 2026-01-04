a29de8d33210a60c5efa090ff594d58b55c363f5
Taylor <johnsoct@hey.com>
2026-01-04 05:52:47 -0500

    finished the getMatrix function

--- 10/10a.ts
+++ 10/10a.ts
@@ -1,3 +1,5 @@
+import { machine } from "os";
+
 import puzzleData from "./10.json" with { type: "json" };
 
 interface MachineData {
@@ -7,6 +9,35 @@ interface MachineData {
 }
 
 const machines = puzzleData.machines;
+
+const getGaussianMatrix = (machineData: MachineData) => {
+    const matrix = new Map<number, number[]>(
+        // Initializes the matrix with the exact number of light indicators set to []
+        Array.from({ length: machineData.lightIndicators.length }, (_, index) => {
+            return [ index, [] ];
+        })
+    );
+    const target = machineData.lightIndicators;
+
+    for (const key of matrix.keys()) {
+        // Parses each button diagram to set the row's columns accordingly
+        machineData.buttonDiagrams.forEach((buttons) => {
+            // If a button diagram toggles a light, set it to 1
+            if (buttons.includes(key)) {
+                matrix.set(key, [ ...matrix.get(key)!, 1 ]);
+            }
+            // If the button diagram doesn't toggle a light, set it to 0
+            else {
+                matrix.set(key, [ ...matrix.get(key)!, 0 ]);
+            }
+        });
+
+        // Set the target as the last index of the row
+        matrix.set(key, [ ...matrix.get(key)!, target[key] ]);
+    }
+
+    return matrix;
+};
 
 const getMachineData = (rawMachineData: string): MachineData => {
     const buttonDiagrams = rawMachineData
@@ -56,8 +87,10 @@ const getMachineData = (rawMachineData: string): MachineData => {
     };
 };
 
-for (const machine of machines.slice(0, 5)) {
+for (const machine of machines.slice(0, 1)) {
     const machineData = getMachineData(machine);
+    const matrix = getGaussianMatrix(machineData);
 
-    console.dir("MachineData", machineData);
+    console.log("MachineData", machineData);
+    console.log("Matrix", matrix);
 }
